d63d95c736124cfea92dd42ba8884342a5a7c056
gary92gs <gary92.gs@gmail.com>
2024-01-17 19:22:28 -0700

    added a return false to solve the code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  console.log(horizontalJoin);
+  for (line of horizontalJoin) {
+      if (line.includes(word)) return true
+  }
+  return false;
 }
 
+
 module.exports = wordSearch
+
+
+// Exercise
+// For this challenge, you'll be implementing a word search solver, 
+// as a function that receives a 2D array of letters and a word. The function must:
+
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
